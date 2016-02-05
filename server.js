0296b5625af221993b3abf6fa36a65f3fd149a53
usmaks <anoly.konz.0099@gmail.com>
2016-02-05 21:10:34 +0900

    Hash and salt password

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-// test
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -118,7 +118,7 @@ app.post('/users', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user){
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
